2d772c1bfaa68bbff891877dcd92242cdf2cbd9d
Rubens Saraiva <rubens.piano@gmail.com>
2013-03-19 09:17:19 -0300

    implementing jpa configurations

--- src/main/java/com/jboss/forge/scaffold/vraptor/app/provider/EntityManagerProvider.java
+++ src/main/java/com/jboss/forge/scaffold/vraptor/app/provider/EntityManagerProvider.java
@@ -4,6 +4,7 @@ import br.com.caelum.vraptor.ComponentRegistry;
 import br.com.caelum.vraptor.ioc.spring.SpringProvider;
 import br.com.caelum.vraptor.util.jpa.EntityManagerCreator;
 import br.com.caelum.vraptor.util.jpa.EntityManagerFactoryCreator;
+import br.com.caelum.vraptor.util.jpa.JPATransactionInterceptor;
 
 public class EntityManagerProvider extends SpringProvider {
 
@@ -11,5 +12,6 @@ public class EntityManagerProvider extends SpringProvider {
     protected void registerCustomComponents(ComponentRegistry registry) {
         registry.register(EntityManagerCreator.class, EntityManagerCreator.class);
         registry.register(EntityManagerFactoryCreator.class, EntityManagerFactoryCreator.class);
+        //registry.register(JPATransactionInterceptor.class, JPATransactionInterceptor.class);
     }
 }
--- src/main/java/com/jboss/forge/scaffold/vraptor/app/view/CustomerController.java
+++ src/main/java/com/jboss/forge/scaffold/vraptor/app/view/CustomerController.java
@@ -1,11 +1,13 @@
 package com.jboss.forge.scaffold.vraptor.app.view;
 
 import br.com.caelum.vraptor.Get;
+import br.com.caelum.vraptor.Path;
+import br.com.caelum.vraptor.Post;
 import br.com.caelum.vraptor.Resource;
 import br.com.caelum.vraptor.Result;
-
 import com.jboss.forge.scaffold.vraptor.app.model.Customer;
 import javax.persistence.EntityManager;
+import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 
@@ -22,48 +24,50 @@ public class CustomerController {
 
     @Get
     public void search() {
-        CriteriaQuery<Customer> criteria = entityManager.getCriteriaBuilder().createQuery(Customer.class);
-        Root<Customer> personRoot = criteria.from(Customer.class);
-        criteria.select(personRoot);
-        result.include("customers", entityManager.createQuery(criteria).getResultList());
+        result.include("customers", entityManager.createQuery("from Customer c").getResultList());
     }
 
-//    @Post
-//    public void search(Customer customer) {
-//
-//        Criteria criteria = session.createCriteria(Customer.class);
-//        if (customer.getFirstName() != null && !"".equals(customer.getFirstName())) {
-//            criteria.add(Restrictions.like("firstName", "%" + customer.getFirstName() + "%"));
-//        }
-//        if (customer.getLastName() != null && !"".equals(customer.getLastName())) {
-//            criteria.add(Restrictions.like("lastName", "%" + customer.getLastName() + "%"));
-//        }
-//        result.include("customers", criteria.list());
-//    }
-//
-//    public void create() {
-//    }
-//
-//    @Path("/customer/view/{customer.id}")
-//    @Get
-//    public void view(Customer customer) {
-//        result.include("customer", session.load(Customer.class, customer.getId()));
-//    }
-//
-//    @Path("/customer/edit/{customer.id}")
-//    @Get
-//    public void edit(Customer customer) {
-//        result.include("customer", session.load(Customer.class, customer.getId()));
-//    }
-//
-//    public void save(Customer customer) {
-//
-//        if (customer.getId() == null) {
-//            session.persist(customer);
-//        } else {
-//            session.merge(customer);
-//        }
-//
-//        result.redirectTo(CustomerController.class).search();
-//    }
+    @Post
+    public void search(Customer customer) {
+        CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Customer> query = builder.createQuery(Customer.class);
+        Root<Customer> from = query.from(Customer.class);
+        CriteriaQuery<Customer> select = query.select(from);
+        
+        if (customer.getFirstName() != null && !"".equals(customer.getFirstName())) {
+            query.where(builder.like(from.<String>get("firstName"), "%" + customer.getFirstName() + "%"));
+        }
+
+        if (customer.getLastName() != null && !"".equals(customer.getLastName())) {
+            query.where(builder.like(from.<String>get("lastName"), "%" + customer.getLastName() + "%"));
+        }
+        
+        result.include("customers", entityManager.createQuery(select).getResultList());
+    }
+
+    public void create() {
+    }
+
+    @Path("/customer/view/{customer.id}")
+    @Get
+    public void view(Customer customer) {
+        result.include("customer", entityManager.find(Customer.class, customer.getId()));
+    }
+
+    @Path("/customer/edit/{customer.id}")
+    @Get
+    public void edit(Customer customer) {
+        result.include("customer", entityManager.find(Customer.class, customer.getId()));
+    }
+
+    public void save(Customer customer) {
+
+        if (customer.getId() == null) {
+            entityManager.persist(customer);
+        } else {
+            entityManager.merge(customer);
+        }
+
+        result.redirectTo(CustomerController.class).search();
+    }
 }
